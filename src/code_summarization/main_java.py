4d089f9705bf985e105ffc242d6b82dea7c41836
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-10-28 10:52:39 +0800

    feat: Java example

---
+++ src/code_summarization/main_java.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from tree_sitter import Language, Parser, Tree
+import tree_sitter_java as java_language
+from tree_sitter_utils import process_tree, print_output
+
+CURDIR = Path(__file__).parent.resolve()
+
+JAVA_DIR = CURDIR / "examples" / "java"
+JAVA_FILE = JAVA_DIR / "Main.java"
+
+
+def main():
+    parser = Parser(Language(java_language.language()))
+
+    with open(JAVA_FILE, "r", encoding="utf-8") as f:
+        code = f.read()
+
+    tree: Tree = parser.parse(bytes(code, "utf8"))
+    output_tree, node_counts = process_tree(tree)
+
+    print("Output tree:")
+    print_output(output_tree)
+
+    print("Node counts:")
+    print_output(node_counts)
+
+
+if __name__ == "__main__":
+    main()
