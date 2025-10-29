fd0381e0be72be05d061e419a7e51ee767a78de0
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-10-29 16:58:44 +0800

    feat: added JavaDoc

--- src/code_summarization/examples/java/Main.java
+++ src/code_summarization/examples/java/Main.java
@@ -1,6 +1,14 @@
 import java.lang.System;
 
 public class Main {
+    /**
+     * Calculates the nth term of the Fibonacci sequence.
+     * Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+     * Each number is the sum of the two preceding ones.
+     *
+     * @param n The term to calculate in the Fibonacci sequence (starting from 0)
+     * @return The nth term of the Fibonacci sequence
+     */
     public static int fibonacci(int n) {
         if (n <= 0) {
             return 0;
@@ -19,8 +27,13 @@ public class Main {
         return b;
     }
 
+    /**
+     * The main function.
+     *
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
-        final int n = 10;
+        final int n = Integer.parseInt(args[0]);
         System.out.printf("fibonacci(%d) = %d\n", n, fibonacci(n));
     }
 }
