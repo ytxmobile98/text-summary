b7abc195535b9013c7fcb976acd72bdd0f839e28
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-10-31 11:06:44 +0800

    feat: java query matching example

--- src/code_summarization/main_java.py
+++ src/code_summarization/main_java.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from tree_sitter import Language, Parser, Tree
+from tree_sitter import Language, Parser, Query, QueryCursor, Tree
 import tree_sitter_java as java_language
+from tree_sitter_queries import JAVA_QUERY
 from tree_sitter_utils import process_tree
 
 CURDIR = Path(__file__).parent.resolve()
@@ -10,16 +11,40 @@ JAVA_FILE = JAVA_DIR / "Main.java"
 
 
 def main():
-    parser = Parser(Language(java_language.language()))
+    language = Language(java_language.language())
+    parser = Parser(language)
 
     with open(JAVA_FILE, "r", encoding="utf-8") as f:
         code = f.read()
 
+    # generate parse tree
+    print("========== PARSE TREE ==========")
     tree: Tree = parser.parse(bytes(code, "utf8"))
     output = process_tree(tree)
-
     output.print_parse_tree()
     output.print_node_counts()
+    print()
+
+    # generate query captures
+    print("========== CAPTURES ==========")
+    query = Query(language, JAVA_QUERY)
+    query_cursor = QueryCursor(query)
+    captures = query_cursor.captures(tree.root_node)
+    for capture_type, nodes in captures.items():
+        for node in nodes:
+            print(f"Capture Type: {capture_type},"
+                  f" Text: {(node.text or b'').decode('utf-8')}")
+    print()
+
+    # generate query matches
+    print("========== MATCHES ==========")
+    matches = query_cursor.matches(tree.root_node)
+    for i, d in matches:
+        print(f"Match {i}:")
+        for match_type, nodes in d.items():
+            for node in nodes:
+                print(f"  Match Type: {match_type},"
+                      f" Text: {(node.text or b'').decode('utf-8')}")
 
 
 if __name__ == "__main__":
---
+++ src/code_summarization/tree_sitter_queries.py
@@ -0,0 +1,34 @@
+JAVA_QUERY = """
+; 顶级结构
+(package_declaration (scoped_identifier) @package)
+
+; 类和接口
+(class_declaration
+    (modifiers)?
+    name: (identifier) @class_name
+    body: (class_body) @class_body_node
+)
+
+; 顶级枚举
+; 我们只捕获枚举的名称，不进入其内部捕获常量
+(enum_declaration
+    (modifiers)?
+    name: (identifier) @enum_name
+    body: (enum_body) @enum_body_node
+)
+
+; 构造函数
+(constructor_declaration
+    (modifiers)?
+    name: (identifier) @method_constructor
+    parameters: (formal_parameters) @method_params
+)
+
+; 方法/函数
+(method_declaration
+    (modifiers)?
+    type: (_)
+    name: (identifier) @method_name
+    parameters: (formal_parameters) @method_params
+)
+"""
