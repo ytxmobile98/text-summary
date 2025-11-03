b0beb1951ad0f2f135dd446c24bb793b1f7123a1
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-11-03 11:12:39 +0800

    feat: revised java query

--- src/code_summarization/tree_sitter_queries.py
+++ src/code_summarization/tree_sitter_queries.py
@@ -4,30 +4,38 @@ JAVA_QUERY = """
 
 ; 类和接口
 (class_declaration
-    (modifiers)? @modifiers
-    name: (identifier) @class_name
+    (
+        (modifiers)?
+        name: (identifier)
+    ) @class_declarator
 )
 
 ; 顶级枚举
 ; 我们只捕获枚举的名称，不进入其内部捕获常量
 (enum_declaration
-    (modifiers)? @modifiers
-    name: (identifier) @enum_name
-    body: (enum_body) @enum_body_node
+    (
+        (modifiers)?
+        name: (identifier)
+        body: (enum_body)
+    ) @enum_declarator
 )
 
 ; 构造函数
 (constructor_declaration
-    (modifiers)? @modifiers
-    name: (identifier) @method_constructor
-    parameters: (formal_parameters) @method_params
+    (
+        (modifiers)?
+        name: (identifier)
+        parameters: (formal_parameters)
+    ) @constructor_declarator
 )
 
 ; 方法/函数
 (method_declaration
-    (modifiers)? @modifiers
-    type: (_unannotated_type) @method_return_type
-    name: (identifier) @method_name
-    parameters: (formal_parameters) @method_params
+    (
+        (modifiers)?
+        type: (_unannotated_type)
+        name: (identifier)
+        parameters: (formal_parameters)
+    ) @method_declarator
 )
 """
